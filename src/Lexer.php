128b5ebb1c9c16a56560d745d1ade8934c03f3b1
Nathan Bishop <nbish11@hotmail.com>
2019-05-22 17:48:06 +1000

    Return a 'token' object-stream

--- src/Lexer.php
+++ src/Lexer.php
@@ -2,12 +2,21 @@
 
 namespace JsonMachine;
 
+use JsonMachine\Token;
+
 class Lexer implements \IteratorAggregate
 {
     /** @var resource */
     private $bytesIterator;
 
-    private $position = 0;
+    /** @var integer */
+    private $position;
+
+    /** @var integer */
+    private $line;
+
+    /** @var integer */
+    private $column;
 
     /**
      * @param \Traversable $bytesIterator
@@ -15,62 +24,106 @@ class Lexer implements \IteratorAggregate
     public function __construct(\Traversable $bytesIterator)
     {
         $this->bytesIterator = $bytesIterator;
+        $this->position = 0;
+        $this->line = 0;
+        $this->column = 0;
     }
-    
+
     /**
      * @return \Generator
      */
     public function getIterator()
     {
-        $inString = false;
-        $tokenBuffer = '';
-        $isEscaping = false;
+        foreach ($this->bytesIterator as $buffer) {
+            for ($i = 0, $l = strlen($buffer); $i < $l; $i++) {
+                $character = $buffer[$i];
 
-        ${' '} = 0;
-        ${"\n"} = 0;
-        ${"\r"} = 0;
-        ${"\t"} = 0;
-        ${'{'} = 1;
-        ${'}'} = 1;
-        ${'['} = 1;
-        ${']'} = 1;
-        ${':'} = 1;
-        ${','} = 1;
+                switch ($character) {
+                    case " ":
+                    case "\t":
+                        break;
 
-        foreach ($this->bytesIterator as $bytes) {
-            $bytesLength = strlen($bytes);
-            for ($i = 0; $i < $bytesLength; ++$i) {
-                $byte = $bytes[$i];
-                ++$this->position;
+                    case "\r":
+                    case "\n":
+                        // advance to next line and reset column
+                        $this->line++;
+                        $this->column = -1;
+                        break;
 
-                if ($inString) {
-                    if ($byte === '"' && !$isEscaping) {
-                        $inString = false;
-                    }
-                    $isEscaping = ($byte =='\\' && !$isEscaping);
-                    $tokenBuffer .= $byte;
-                    continue;
+                    case '{':
+                        yield new Token($this->line, $this->column, Token::OBJECT_START, '{');
+                        break;
+
+                    case '}':
+                        yield new Token($this->line, $this->column, Token::OBJECT_END, '}');
+                        break;
+
+                    case '[':
+                        yield new Token($this->line, $this->column, Token::ARRAY_START, '[');
+                        break;
+
+                    case ']':
+                        yield new Token($this->line, $this->column, Token::ARRAY_END, ']');
+                        break;
+
+                    case ':':
+                        yield new Token($this->line, $this->column, Token::COLON, ':');
+                        break;
+
+                    case ',':
+                        yield new Token($this->line, $this->column, Token::COMMA, ',');
+                        break;
+
+                    case '"':
+                        $value = $character;
+                        $width = 0;
+                        $escaping = false;
+
+                        while (true) {
+                            $char = $buffer[++$i];
+                            $value .= $char;
+                            $width++;
+
+                            if (($char === '"' && !$escaping) || $char === '') {
+                                break;
+                            }
+
+                            $escaping = ($char === '\\' && !$escaping); // deals with escaped back slashes
+                        }
+
+                        yield new Token($this->line, $this->column, Token::SCALAR_STRING, $value);
+
+                        $this->column += $width;
+
+                        break;
+
+                    default:
+                        $value = $character;
+                        $width = 0;
+
+                        while (true) {
+                            $char = isset($buffer[++$i]) ? $buffer[$i] : '';
+
+                            if (in_array($char, ['{', '}', '[', ']', ':', ',', '"', " ", "\t", "\r", "\n", ''])) {
+                                $i--;  // let the outer loop handle these characters
+
+                                break;
+                            }
+
+                            $value .= $char;
+                            $width++;
+                        }
+
+                        yield new Token($this->line, $this->column, Token::SCALAR_CONST, $value);
+
+                        $this->column += $width;
                 }
 
-                if (isset($$byte)) {
-                    if ($tokenBuffer !== '') {
-                        yield $tokenBuffer;
-                        $tokenBuffer = '';
-                    }
-                    if ($$byte) { // is not whitespace
-                        yield $byte;
-                    }
-                } else {
-                    if ($byte === '"') {
-                        $inString = true;
-                    }
-                    $tokenBuffer .= $byte;
-                }
+                $this->column++;
             }
         }
-        if ($tokenBuffer !== '') {
-            yield $tokenBuffer;
-        }
+
+        $this->position = $this->column;
     }
 
     /**
--- test/JsonMachineTest/LexerTest.php
+++ test/JsonMachineTest/LexerTest.php
@@ -11,11 +11,24 @@ class LexerTest extends \PHPUnit_Framework_TestCase
     {
         $data = ['{}[],:null,"string" false:', 'true,1,100000,1.555{-56]"","\\""'];
         $expected = ['{','}','[',']',',',':','null',',','"string"','false',':','true',',','1',',','100000',',','1.555','{','-56',']','""',',','"\\""'];
-        $this->assertEquals($expected, iterator_to_array(new Lexer(new \ArrayIterator($data))));
+        $lexer = new Lexer(new \ArrayIterator($data));
+
+        $this->assertEquals($expected, $this->flattenToLexemes($lexer));
     }
 
     public function testCorrectlyParsesTwoBackslashesAtTheEndOfAString()
     {
-        $this->assertEquals(['"test\\\\"', ':'], iterator_to_array(new Lexer(new \ArrayIterator(['"test\\\\":']))));
+        $lexer = new Lexer(new \ArrayIterator(['"test\\\\":']));
+
+        $this->assertEquals(['"test\\\\"', ':'], $this->flattenToLexemes($lexer));
+    }
+
+    private function flattenToLexemes(Lexer $lexer)
+    {
+        $tokens = iterator_to_array($lexer);
+
+        return array_map(function ($token) {
+            return $token->getValue();
+        }, $tokens);
     }
 }
