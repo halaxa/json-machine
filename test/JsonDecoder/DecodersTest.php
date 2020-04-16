09990382a02f22134ef96d7d59ae99e1f7f00ea6
Filip Halaxa <filip@halaxa.cz>
2020-04-16 22:32:29 +0200

    DecodersTest

--- test/JsonDecoder/DecodersTest.php
+++ test/JsonDecoder/DecodersTest.php
@@ -1,36 +1,37 @@
 <?php
 
 use JsonMachine\JsonDecoder\ExtJsonDecoder;
+use JsonMachine\JsonDecoder\PassThruDecoder;
 
-class ExtJsonDecoderTest extends PHPUnit_Framework_TestCase
+class DecodersTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider dataPassesOptionsToJsonDecode
      */
-    public function testPassesOptionsToJsonDecode($methodName)
+    public function testPassesOptionsToJsonDecode($className, $methodName)
     {
         $json = '{"bigint": 123456789123456789123456789, "deep": [["deeper"]]}';
 
-        $defaultDecoder = new ExtJsonDecoder();
+        $defaultDecoder = new $className();
         $defaultResult = $defaultDecoder->$methodName($json);
         $this->assertInternalType("object", $defaultResult->getValue());
         $this->assertNotInternalType("string", $defaultResult->getValue()->bigint);
         $this->assertSame([["deeper"]], $defaultResult->getValue()->deep);
 
-        $assocDecoder = new ExtJsonDecoder(true);
+        $assocDecoder = new $className(true);
         $assocResult = $assocDecoder->$methodName($json);
         $this->assertInternalType("array", $assocResult->getValue());
 
-        $objDecoder = new ExtJsonDecoder(false);
+        $objDecoder = new $className(false);
         $objResult = $objDecoder->$methodName($json);
         $this->assertInternalType("object", $objResult->getValue());
 
-        $depthDecoder = new ExtJsonDecoder(true, 1);
+        $depthDecoder = new $className(true, 1);
         $depthResult = $depthDecoder->$methodName($json);
         $this->assertFalse($depthResult->isOk());
         $this->assertSame("Maximum stack depth exceeded", $depthResult->getErrorMessage());
 
-        $bigintDecoder = new ExtJsonDecoder(null, 1, JSON_BIGINT_AS_STRING);
+        $bigintDecoder = new $className(null, 1, JSON_BIGINT_AS_STRING);
         $bigintResult = $bigintDecoder->$methodName("123123123123123123123");
         $this->assertSame("123123123123123123123", $bigintResult->getValue());
     }
@@ -38,8 +39,9 @@ class ExtJsonDecoderTest extends PHPUnit_Framework_TestCase
     public function dataPassesOptionsToJsonDecode()
     {
         return [
-            ['decodeKey'],
-            ['decodeValue'],
+            [PassThruDecoder::class, 'decodeKey'],
+            [ExtJsonDecoder::class, 'decodeKey'],
+            [ExtJsonDecoder::class, 'decodeValue'],
         ];
     }
 }
