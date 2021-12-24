1cdb150acf3998985d4a76cd6fc68f6e93f93cb3
Filip Halaxa <filip@halaxa.cz>
2021-12-24 12:48:28 +0100

    Kept previous API for JsonMachine class

--- src/JsonMachine.php
+++ src/JsonMachine.php
@@ -32,31 +32,21 @@ class JsonMachine implements \IteratorAggregate, PositionAware
     private $parser;
 
     /**
-     * @var bool
-     */
-    private $debugEnabled;
-
-    /**
      * @param iterable $bytesIterator
      * @param string $jsonPointer
      * @param Decoder|ChunkDecoder $jsonDecoder
-     * @param bool $debugEnabled
      * @throws Exception\InvalidArgumentException
      */
-    public function __construct($bytesIterator, $jsonPointer = '', $jsonDecoder = null, $debugEnabled = true)
+    public function __construct($bytesIterator, $jsonPointer = '', $jsonDecoder = null)
     {
         @trigger_error("Class JsonMachine is deprecated. Use class Items instead.", E_USER_DEPRECATED);
 
         $this->bytesIterator = $bytesIterator;
         $this->jsonPointer = $jsonPointer;
         $this->jsonDecoder = $jsonDecoder;
-        $this->debugEnabled = $debugEnabled;
 
         $this->parser = new Parser(
-            new Lexer(
-                $this->bytesIterator,
-                $this->debugEnabled
-            ),
+            new Lexer($this->bytesIterator,true),
             $this->jsonPointer,
             $this->jsonDecoder ?: new ExtJsonDecoder(true)
         );
@@ -66,70 +56,58 @@ class JsonMachine implements \IteratorAggregate, PositionAware
      * @param string $string
      * @param string $jsonPointer
      * @param Decoder|ChunkDecoder $jsonDecoder
-     * @param bool $debugEnabled
      * @return self
      * @throws Exception\InvalidArgumentException
      */
-    public static function fromString($string, $jsonPointer = '', $jsonDecoder = null, $debugEnabled = true)
+    public static function fromString($string, $jsonPointer = '', $jsonDecoder = null)
     {
-        return new static(new StringChunks($string), $jsonPointer, $jsonDecoder, $debugEnabled);
+        return new static(new StringChunks($string), $jsonPointer, $jsonDecoder);
     }
 
     /**
      * @param string $file
      * @param string $jsonPointer
      * @param Decoder|ChunkDecoder $jsonDecoder
-     * @param bool $debugEnabled
      * @return self
      * @throws Exception\InvalidArgumentException
      */
-    public static function fromFile($file, $jsonPointer = '', $jsonDecoder = null, $debugEnabled = true)
+    public static function fromFile($file, $jsonPointer = '', $jsonDecoder = null)
     {
-        return new static(new FileChunks($file), $jsonPointer, $jsonDecoder, $debugEnabled);
+        return new static(new FileChunks($file), $jsonPointer, $jsonDecoder);
     }
 
     /**
      * @param resource $stream
      * @param string $jsonPointer
      * @param Decoder|ChunkDecoder $jsonDecoder
-     * @param bool $debugEnabled
      * @return self
      * @throws Exception\InvalidArgumentException
      */
-    public static function fromStream($stream, $jsonPointer = '', $jsonDecoder = null, $debugEnabled = true)
+    public static function fromStream($stream, $jsonPointer = '', $jsonDecoder = null)
     {
-        return new static(new StreamChunks($stream), $jsonPointer, $jsonDecoder, $debugEnabled);
+        return new static(new StreamChunks($stream), $jsonPointer, $jsonDecoder);
     }
 
     /**
      * @param iterable $iterable
      * @param string $jsonPointer
      * @param Decoder|ChunkDecoder $jsonDecoder
-     * @param bool $debugEnabled
      * @return self
      * @throws Exception\InvalidArgumentException
      */
-    public static function fromIterable($iterable, $jsonPointer = '', $jsonDecoder = null, $debugEnabled = true)
+    public static function fromIterable($iterable, $jsonPointer = '', $jsonDecoder = null)
     {
-        return new static($iterable, $jsonPointer, $jsonDecoder, $debugEnabled);
+        return new static($iterable, $jsonPointer, $jsonDecoder);
     }
 
     #[\ReturnTypeWillChange]
     public function getIterator()
     {
-        return $this->parser->getIterator();
+        return $this->parser;
     }
 
     public function getPosition()
     {
         return $this->parser->getPosition();
-    }
-
-    /**
-     * @return bool
-     */
-    public function isDebugEnabled()
-    {
-        return $this->debugEnabled;
     }
 }
