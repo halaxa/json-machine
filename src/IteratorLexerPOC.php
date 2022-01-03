e5502121489ed27e9937eb2aeec84c711e3d5057
Filip Halaxa <filip@halaxa.cz>
2022-01-03 21:28:34 +0100

    IteratorLexerPOC

---
+++ src/IteratorLexerPOC.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace JsonMachine;
+
+use Iterator;
+
+class IteratorLexerPOC implements \Iterator
+{
+    /** @var Iterator */
+    private $jsonChunks;
+
+
+    /** @var array */
+    private $tokenBoundaries = [];
+
+    /** @var array  */
+    private $nonSignificantBytes = [];
+
+
+    /** @var array */
+    private $tokens = [];
+
+    /** @var string */
+    private $currentToken = '';
+
+    /** @var int */
+    private $currentTokenKey = -1;
+
+    /** @var string */
+    private $chunk = '';
+
+    /** @var int */
+    private $chunkLength = 0;
+
+    /** @var int */
+    private $chunkIndex = 0;
+
+
+    /** @var bool */
+    private $inString = false;
+
+    /** @var string */
+    private $tokenBuffer = '';
+
+    /** @var bool */
+    private $escaping = false;
+
+
+    /**
+     * @param Iterator<string> $jsonChunks
+     */
+    public function __construct(Iterator $jsonChunks)
+    {
+        $this->jsonChunks = $jsonChunks;
+    }
+
+
+    public function rewind()
+    {
+        $this->tokenBoundaries = $this->mapOfBoundaryBytes();
+        $this->nonSignificantBytes = $this->mapOfAllBytes();
+
+        if ($this->advanceToNextJsonChunk()) {
+            $this->next();
+        }
+    }
+
+
+    public function next()
+    {
+
+        for ( ; $this->chunkIndex < $this->chunkLength; ++$this->chunkIndex) {
+
+            if ($this->tokens) {
+                $this->currentToken = array_shift($this->tokens);
+                ++$this->currentTokenKey;
+                return;
+            }
+
+            $byte = $this->chunk[$this->chunkIndex];
+
+            if ($this->escaping) {
+                $this->escaping = false;
+                $this->tokenBuffer .= $byte;
+                continue;
+            }
+
+            if ($this->nonSignificantBytes[$byte]) {
+                $this->tokenBuffer .= $byte;
+                continue;
+            }
+
+            if ($this->inString) {
+                if ($byte == '"') {
+                    $this->inString = false;
+                } elseif ($byte == '\\') {
+                    $this->escaping = true;
+                }
+                $this->tokenBuffer .= $byte;
+                continue;
+            }
+
+            if (isset($this->tokenBoundaries[$byte])) { // if byte is any token boundary
+                $this->flushTokenBuffer();
+                if ($this->tokenBoundaries[$byte]) { // if byte is not whitespace token boundary
+                    $this->tokens[] = $byte;
+                }
+                return;
+            } else {
+                if ($byte == '"') {
+                    $this->inString = true;
+                }
+                $this->tokenBuffer .= $byte;
+            }
+        }
+
+        if (! $this->advanceToNextJsonChunk()) {
+            $this->flushTokenBuffer();
+        }
+    }
+
+
+    public function valid()
+    {
+        return $this->tokens != [];
+    }
+
+
+    public function current()
+    {
+        return $this->currentToken;
+    }
+
+
+    public function key()
+    {
+        return $this->currentTokenKey;
+    }
+
+
+    private function mapOfBoundaryBytes(): array
+    {
+        $utf8bom = "\xEF\xBB\xBF";
+
+        $boundary = [];
+        $boundary[$utf8bom[0]] = 0;
+        $boundary[$utf8bom[1]] = 0;
+        $boundary[$utf8bom[2]] = 0;
+        $boundary[' ']         = 0;
+        $boundary["\n"]        = 0;
+        $boundary["\r"]        = 0;
+        $boundary["\t"]        = 0;
+
+        $boundary['{']         = 1;
+        $boundary['}']         = 1;
+        $boundary['[']         = 1;
+        $boundary[']']         = 1;
+        $boundary[':']         = 1;
+        $boundary[',']         = 1;
+
+        return $boundary;
+    }
+
+
+    private function mapOfAllBytes(): array
+    {
+        $allBytes = [];
+        foreach (range(0, 255) as $ord) {
+            $allBytes[chr($ord)] = !in_array(
+                chr($ord),
+                ["\\", '"', "\xEF", "\xBB", "\xBF", ' ', "\n", "\r", "\t", '{', '}', '[', ']', ':', ',']
+            );
+        }
+
+        return $allBytes;
+    }
+
+
+    private function advanceToNextJsonChunk(): bool
+    {
+        if ($this->currentTokenKey > -1) {
+            $this->jsonChunks->next();
+        } else {
+            $this->jsonChunks->rewind();
+        }
+
+        if ($this->jsonChunks->valid()) {
+            $this->chunk = $this->jsonChunks->current();
+            $this->chunkLength = strlen($this->chunk);
+            $this->chunkIndex = 0;
+            $this->jsonChunks->key();
+        }
+
+        return $this->jsonChunks->valid();
+    }
+
+
+    private function flushTokenBuffer()
+    {
+        if ($this->tokenBuffer != '') {
+            $this->tokens[] = $this->tokenBuffer;
+            $this->tokenBuffer = '';
+        }
+    }
+
+
+    public function getPosition(): int
+    {
+        return 0;
+    }
+
+
+    public function getLine(): int
+    {
+        return 1;
+    }
+
+
+    public function getColumn(): int
+    {
+        return 0;
+    }
+}
