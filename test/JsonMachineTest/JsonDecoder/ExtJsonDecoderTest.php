0679692b3e08f2f84012b19c053b23d0c097ba35
Filip Halaxa <filip@halaxa.cz>
2022-02-03 14:15:05 +0100

    Separate test cases for individual options

--- test/JsonMachineTest/JsonDecoder/ExtJsonDecoderTest.php
+++ test/JsonMachineTest/JsonDecoder/ExtJsonDecoderTest.php
@@ -7,11 +7,10 @@ use PHPUnit_Framework_TestCase;
 
 /**
  * @covers \JsonMachine\JsonDecoder\ExtJsonDecoder
- * @covers \JsonMachine\JsonDecoder\PassThruDecoder
  */
 class ExtJsonDecoderTest extends PHPUnit_Framework_TestCase
 {
-    public function testPassesOptionsToJsonDecode()
+    public function testDefaultOptions()
     {
         $json = '{"bigint": 123456789123456789123456789, "deep": [["deeper"]]}';
 
@@ -20,20 +19,38 @@ class ExtJsonDecoderTest extends PHPUnit_Framework_TestCase
         $this->assertTrue('object' === gettype($defaultResult->getValue()));
         $this->assertFalse('string' === gettype($defaultResult->getValue()->bigint));
         $this->assertSame([['deeper']], $defaultResult->getValue()->deep);
+    }
+
+    public function testPassesAssocTrueOptionToJsonDecode()
+    {
+        $json = '{"bigint": 123456789123456789123456789, "deep": [["deeper"]]}';
 
         $assocDecoder = new ExtJsonDecoder(true);
         $assocResult = $assocDecoder->decode($json);
         $this->assertTrue('array' === gettype($assocResult->getValue()));
+    }
+
+    public function testPassesAssocFalseOptionToJsonDecode()
+    {
+        $json = '{"bigint": 123456789123456789123456789, "deep": [["deeper"]]}';
 
         $objDecoder = new ExtJsonDecoder(false);
         $objResult = $objDecoder->decode($json);
         $this->assertTrue('object' === gettype($objResult->getValue()));
+    }
+
+    public function testPassesPassesDepthOptionToJsonDecode()
+    {
+        $json = '{"bigint": 123456789123456789123456789, "deep": [["deeper"]]}';
 
         $depthDecoder = new ExtJsonDecoder(true, 1);
         $depthResult = $depthDecoder->decode($json);
         $this->assertFalse($depthResult->isOk());
         $this->assertSame('Maximum stack depth exceeded', $depthResult->getErrorMessage());
+    }
 
+    public function testPassesPassesBigIntOptionToJsonDecode()
+    {
         $bigintDecoder = new ExtJsonDecoder(null, 1, JSON_BIGINT_AS_STRING);
         $bigintResult = $bigintDecoder->decode('123123123123123123123');
         $this->assertSame('123123123123123123123', $bigintResult->getValue());
