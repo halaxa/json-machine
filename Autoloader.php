6fc8f8f14a3844d2852529ab1c1c1c175280fc91
Filip Halaxa <filip@halaxa.cz>
2022-02-12 14:04:22 +0100

    make cs-fix

--- Autoloader.php
+++ Autoloader.php
@@ -1,6 +1,8 @@
 <?php
+
+declare(strict_types=1);
 /**
- * A simple PSR-4 spec auto loader to allow json-machine to function the same as if it were loaded via Composer
+ * A simple PSR-4 spec auto loader to allow json-machine to function the same as if it were loaded via Composer.
  *
  * To use this just include this file in your script and the JsonMachine namespace will be made available
  *
@@ -8,16 +10,16 @@
  *
  * See: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
  *
- * @param string $class The fully-qualified class name.
+ * @param string $class the fully-qualified class name
+ *
  * @return void
  */
 spl_autoload_register(function ($class) {
-
     // project-specific namespace prefix
     $prefix = 'JsonMachine\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+    $base_dir = __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR;
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
@@ -32,7 +34,7 @@ spl_autoload_register(function ($class) {
     // replace the namespace prefix with the base directory, replace namespace
     // separators with directory separators in the relative class name, append
     // with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
 
     // if the file exists, require it
     if (file_exists($file)) {
