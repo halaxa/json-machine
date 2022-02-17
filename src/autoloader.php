f064f4ca61e5f16484d06de2e73ef6a10c957eee
Filip Halaxa <filip@halaxa.cz>
2022-02-17 19:09:55 +0100

    Better Autoloading readability

--- src/autoloader.php
+++ src/autoloader.php
@@ -21,28 +21,17 @@ class Autoloading
 {
     static public function autoloader($class)
     {
-        // project-specific namespace prefix
         $prefix = 'JsonMachine\\';
+        $baseDir = __DIR__.DIRECTORY_SEPARATOR;
 
-        // base directory for the namespace prefix
-        $base_dir = __DIR__.DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR;
-
-        // does the class use the namespace prefix?
         $len = strlen($prefix);
         if (strncmp($prefix, $class, $len) !== 0) {
-            // no, move to the next registered autoloader
             return;
         }
 
-        // get the relative class name
-        $relative_class = substr($class, $len);
+        $classWithoutPrefix = substr($class, $len);
+        $file = $baseDir.str_replace('\\', '/', $classWithoutPrefix).'.php';
 
-        // replace the namespace prefix with the base directory, replace namespace
-        // separators with directory separators in the relative class name, append
-        // with .php
-        $file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
-
-        // if the file exists, require it
         if (file_exists($file)) {
             require $file;
         }
