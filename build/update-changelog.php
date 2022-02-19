91821315aaf4740dcaa39b1f8a50fd44f047bd5f
Filip Halaxa <filip@halaxa.cz>
2022-02-19 20:01:29 +0100

    `make cs-fix`

--- build/update-changelog.php
+++ build/update-changelog.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 $version = $argv[1];
 $changelogPath = $argv[2];
 
 $changelogContents = file_get_contents($changelogPath);
 if (false !== strpos($changelogContents, $version)) {
     echo "Version $version already in $changelogPath. Stopping.";
-    exit (1);
+    exit(1);
 }
 
-$releaseDate = date("Y-m-d");
-$changelogMatch = "## master";
+$releaseDate = date('Y-m-d');
+$changelogMatch = '## master';
 $changelogReplace = "$changelogMatch
 Nothing yet
 
@@ -19,7 +21,3 @@ Nothing yet
 ## $version - $releaseDate";
 
 file_put_contents($changelogPath, str_replace($changelogMatch, $changelogReplace, $changelogContents));
-
-# commit
-
-# push
