69bb567a512de8a6f7ccb969a48430ce708310c2
Filip Halaxa <filip@halaxa.cz>
2023-05-05 09:40:42 +0200

    Feeding jsonmachine_next_token multiple json chunks

--- test/JsonMachineTest/ExtJsonmachineTest.php
+++ test/JsonMachineTest/ExtJsonmachineTest.php
@@ -15,13 +15,19 @@ class ExtJsonmachineTest extends TestCase
         $escaping = false;
         $tokenBuffer = "";
 
-        while($token = jsonmachine_next_token("[{\"one\": 1}, {\"two\": false}, {\"thr\\\"ee\": \"string\"}]", $tokenBuffer, $escaping, $inString, $lastIndex)) {
-            var_dump($token, $tokenBuffer, $escaping, $inString, $lastIndex);
+        foreach (["[{\"one\": 1}, {\"two\": false}, {\"thr\\", "\"ee\": \"string\"}]"] as $chunk) {
+
+            while($token = jsonmachine_next_token($chunk, $tokenBuffer, $escaping, $inString, $lastIndex)) {
+//                var_dump($token, $tokenBuffer, $escaping, $inString, $lastIndex);
+                var_dump($token);
+                flush();
+                ob_flush();
+            }
+
+//            var_dump($token, $tokenBuffer, $escaping, $inString, $lastIndex);
+            var_dump($token);
             flush();
             ob_flush();
         }
-        var_dump($token, $tokenBuffer, $escaping, $inString, $lastIndex);
-        flush();
-        ob_flush();
     }
 }
