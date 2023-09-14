403dcbc26e25625cda70fccd830e6fa2508e5d86
Anton Ukhanev <xedin.unknown@gmail.com>
2023-09-14 12:54:42 +0200

    `testToArrayReturnsJsonPointers()` now uses a data provider

--- test/JsonMachineTest/ValidJsonPointersTest.php
+++ test/JsonMachineTest/ValidJsonPointersTest.php
@@ -70,11 +70,21 @@ class ValidJsonPointersTest extends \PHPUnit_Framework_TestCase
         (new ValidJsonPointers(['/one', '/one']))->toArray();
     }
 
-    public function testToArrayReturnsJsonPointers()
+    /**
+     * @dataProvider data_testToArrayReturnsJsonPointers
+     */
+    public function testToArrayReturnsJsonPointers(string $pointerA, string $pointerB)
     {
         $this->assertSame(
-            ['/one', '/two'],
-            (new ValidJsonPointers(['/one', '/two']))->toArray()
+            [$pointerA, $pointerB],
+            (new ValidJsonPointers([$pointerA, $pointerB]))->toArray()
         );
+    }
+
+    public function data_testToArrayReturnsJsonPointers()
+    {
+        return [
+            ['/one', '/two'],
+        ];
     }
 }
