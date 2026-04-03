03d006d2994872ab8a70b70a7c3d7f0a52493c21
Filip Halaxa <filip@halaxa.cz>
2026-04-03 18:51:05 +0200

    build: Hardcoded PHP patch versions for running tests instead of getting the latest dynamically from hub.docker.com every time.

--- build/build-image.sh
+++ build/build-image.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-PHP_MINOR=$1
-PHP_VERSION=$( (wget -qO- "https://hub.docker.com/v2/repositories/library/php/tags?page_size=100&name=$PHP_MINOR" \
-  | grep -Po "[0-9]+\.[0-9]+\.[0-9]+(?=-)" \
-    || echo "$PHP_MINOR") \
-  | head -1 \
-)
+PHP_VERSION=$1
 XDEBUG_VERSION=$2
-
 
 FROM_IMAGE="php:$PHP_VERSION-cli-alpine"
 CONTAINER_NAME="json-machine-php-$PHP_VERSION"
-
 
 docker ps --all --format "{{.Names}}" | grep "$CONTAINER_NAME" && docker rm -f "$CONTAINER_NAME"
 
